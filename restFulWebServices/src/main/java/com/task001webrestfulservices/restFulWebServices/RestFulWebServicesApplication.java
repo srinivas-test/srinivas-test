74ae063712050c239736abc5982ab190fa003bf2
tirumalasrinivas <75420911+tirumalasrinivas@users.noreply.github.com>
2021-09-03 14:09:45 +0530

    new rest api

---
+++ restFulWebServices/src/main/java/com/task001webrestfulservices/restFulWebServices/RestFulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.task001webrestfulservices.restFulWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulWebServicesApplication.class, args);
+	}
+
+}
---
+++ restFulWebServices/src/test/java/com/task001webrestfulservices/restFulWebServices/RestFulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task001webrestfulservices.restFulWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
